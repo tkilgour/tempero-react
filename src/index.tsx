7379cb835ee8ca9d8e7101d4f7f84f6d8730162f
Thomas Kilgour <thomas@kilgour.co>
2022-04-12 20:36:57 -0400

    create store

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StateProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StateProvider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
